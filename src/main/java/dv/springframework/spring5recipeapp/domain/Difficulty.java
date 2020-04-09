24386da80c0f191436760f21e1cdcb209b0e7fb9
dvanonihupac <dvanoni@hupac.com>
2020-04-10 00:05:29 +0200

    Added Difficuty as JPA enumeration

---
+++ src/main/java/dv/springframework/spring5recipeapp/domain/Difficulty.java
@@ -0,0 +1,6 @@
+package dv.springframework.spring5recipeapp.domain;
+
+public enum Difficulty {
+
+    EASY, MODERATE, KIND_OF_HARD, HARD
+}
--- src/main/java/dv/springframework/spring5recipeapp/domain/Recipe.java
+++ src/main/java/dv/springframework/spring5recipeapp/domain/Recipe.java
@@ -17,11 +17,12 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    //todo add
-    //private Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @Lob
     private Byte[] image;
@@ -117,5 +118,11 @@ public class Recipe {
         this.ingredients = ingredients;
     }
 
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
 
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
 }
