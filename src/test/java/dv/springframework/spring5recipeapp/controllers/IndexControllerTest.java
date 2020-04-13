943f7536280f527d6cb51d588931dce9c8882d1d
dvanonihupac <dvanoni@hupac.com>
2020-04-13 12:38:00 +0200

    Added Junit and Mockito mocks for IndexController, Category and RecipeServiceImpl

---
+++ src/test/java/dv/springframework/spring5recipeapp/controllers/IndexControllerTest.java
@@ -0,0 +1,61 @@
+package dv.springframework.spring5recipeapp.controllers;
+
+import dv.springframework.spring5recipeapp.domain.Recipe;
+import dv.springframework.spring5recipeapp.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    IndexController controller;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        controller = new IndexController(recipeService);
+    }
+
+    @Test
+    void getIndexPage() {
+
+        //given
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeSet.add(new Recipe());
+
+        //recipeSet.add(new Recipe()); If I use again it without assign a new ID the equals method doesn't add the second recipe and the last assertEquals fails
+        Recipe recipe2 = new Recipe();
+        recipe2.setId(1L);
+        recipeSet.add(recipe2);
+
+        Mockito.when(recipeService.getRecipes()).thenReturn(recipeSet);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
+        String viewname = controller.getIndexPage(model);
+
+        //then
+        assertEquals("index", viewname);
+        Mockito.verify(recipeService, Mockito.times(1)).getRecipes();
+        Mockito.verify(model, Mockito.times(1)).addAttribute(Mockito.eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2,setInController.size());
+
+    }
+}
