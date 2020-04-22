3b9f233c63ea24dc578d512e66d185ab88c0ac30
dvanonihupac <dvanoni@hupac.com>
2020-04-22 10:36:57 +0200

    Added Spring MVC Exception Handler for Recipe Controller and 404error.html page

--- src/main/java/dv/springframework/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/dv/springframework/spring5recipeapp/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package dv.springframework.spring5recipeapp.controllers;
 
 import dv.springframework.spring5recipeapp.commands.RecipeCommand;
+import dv.springframework.spring5recipeapp.exceptions.NotFoundException;
 import dv.springframework.spring5recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -48,6 +51,17 @@ public class RecipeController {
         log.debug("Deleting id: " + id);
         recipeService.deleteByid(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(Exception exception){
+        log.error("Handling not found exception");
+        log.error(exception.getMessage());
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
+        return modelAndView;
     }
 
 }
--- src/main/java/dv/springframework/spring5recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/dv/springframework/spring5recipeapp/services/RecipeServiceImpl.java
@@ -40,8 +40,7 @@ public class RecipeServiceImpl implements RecipeService {
     public Recipe findById(Long id) {
         Optional<Recipe> recipeOptional = recipeRepository.findById(id);
         if(!recipeOptional.isPresent()){
-            //throw new RuntimeException("Recipe not found");
-            throw new NotFoundException("Recipe not found");
+            throw new NotFoundException("Recipe not found for ID value " + id.toString());
         }
         return recipeOptional.get();
     }
